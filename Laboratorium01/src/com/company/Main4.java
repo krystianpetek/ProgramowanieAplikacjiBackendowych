e1bc0343ebf814e0850ad2a8f4a13e84a7f7d1f8
Krystian Petek <80618899+krystianpetek@users.noreply.github.com>
2022-03-12 17:51:04 +0100

    Add files via upload

---
+++ Laboratorium01/src/com/company/Main4.java
@@ -0,0 +1,25 @@
+package com.company;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main4 {
+    public static void main(String[] args) throws IOException
+    {
+        FileInputStream stream = new FileInputStream("file.txt");
+        InputStreamReader reader = new InputStreamReader(stream);
+
+        char[] characters = new char[100];
+        reader.read(characters,0,characters.length);
+
+        System.out.println();
+
+//        try {
+//            byte[] data = Files.readAllBytes(Paths.get("file.txt"));
+//            String text = new String(data, StandardCharsets.UTF_8);
+//            System.out.println(text);
+//        } catch (IOException e) {
+//            e.printStackTrace( );
+//        }
+    }
+}
