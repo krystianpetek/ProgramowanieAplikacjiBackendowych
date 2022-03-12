ac26aa95b1909dd4d4e0be4ed6c1e1ad5b3e2de9
Krystian Petek <80618899+krystianpetek@users.noreply.github.com>
2022-03-12 16:47:19 +0100

    Add files via upload

---
+++ zadanie3_1/src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] array = { 4, 5, 7, 11, 12, 15, 15, 21, 40, 45 };
+
+        System.out.println(searchIndex(array,12));
+    }
+
+    public static int searchIndex(int[] tablica, int value)
+    {
+        int index = 0;
+        int limit = tablica.length - 1;
+        while (index <= limit) {
+            int point = (int) Math.ceil((index + limit) / 2);
+            int entry = tablica[point];
+            if (value > entry) {
+                index = point + 1;
+                continue;
+            }
+            if (value < entry) {
+                limit = point - 1;
+                continue;
+            }
+            return point; // value == entry
+        }
+        return -1;
+    }
+
+}
+
