baf487677c297da685e648278f0567554a1e0170
Krystian Petek <krystianpetek2@gmail.com>
2022-03-13 10:57:14 +0100

    commiy

--- Laboratorium01/src/Main2.java
+++ Laboratorium01/src/Main2.java
@@ -1,36 +1,32 @@
-//package com.company;
-//
-//public class Main2 {
-//
-//    public static void main(String[] args)
-//    {
-//        int[] array = {4,5,7,11,12,15,15,21,40,45};
-//        int index = searchIndex(array, 11); // we want to find index for 11
-//        System.out.println(index);
-//    }
-//
-//
-//        public static int split (int index,int limit){
-//            if (index > limit) {
-//                return -1;
-//            }
-//            if (index > limit) {
-//                return -1;
-//            }
-//            int point = (int) Math.ceil((limit + index) / 2.0);
-//            int entry = array[point];
-//            if (value < entry) {
-//                return split(index, point - 1);
-//            }
-//            if (value > entry) {
-//                return split(point + 1, limit);
-//            }
-//            return point; // value == entry
-//        }
-//
-//    public static int searchIndex(int[] array,int value) {
-//        return split(0, array.length - 1);
-//        }
-//
-//
-//}
+public class Main2 {
+
+    public static void main(String[] args) {
+        int[] array = {4, 5, 7, 11, 12, 15, 15, 21, 40, 45};
+        int index = searchIndex(array, 11); // we want to find index for 11
+        System.out.println(index);
+    }
+
+    public static int split(int index, int limit, int[] array, int value) {
+        if (index > limit) {
+            return -1;
+        }
+        if (index > limit) {
+            return -1;
+        }
+        int point = (int) Math.ceil((limit + index) / 2.0);
+        int entry = array[point];
+        if (value < entry) {
+            return split(index, point - 1, array, value);
+        }
+        if (value > entry) {
+            return split(point + 1, limit, array, value);
+        }
+        return point; // value == entry
+    }
+
+    public static int searchIndex(int[] array, int value) {
+        return split(0, array.length - 1, array, value);
+    }
+
+
+}
--- Laboratorium01/src/Main4.java
+++ Laboratorium01/src/Main4.java
@@ -1,23 +1,25 @@
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Main4 {
     public static void main(String[] args) throws IOException {
-        FileInputStream stream = new FileInputStream("Laboratorium01/file.txt");
-        InputStreamReader reader = new InputStreamReader(stream);
 
-        char[] characters = new char[100];
+        FileInputStream stream = new FileInputStream("Laboratorium01/src/file.txt");
+        InputStreamReader reader = new InputStreamReader(stream, StandardCharsets.UTF_8);
+        char[] characters = new char[20];
         reader.read(characters, 0, characters.length);
+        System.out.println(characters);
 
-        System.out.println();
-
-//        try {
-//            byte[] data = Files.readAllBytes(Paths.get("file.txt"));
-//            String text = new String(data, StandardCharsets.UTF_8);
-//            System.out.println(text);
-//        } catch (IOException e) {
-//            e.printStackTrace( );
-//        }
+        try {
+            byte[] data = Files.readAllBytes(Paths.get("Laboratorium01/src/file.txt"));
+            String text = new String(data, StandardCharsets.UTF_8);
+            System.out.println(text);
+        } catch (IOException e) {
+            e.printStackTrace( );
+        }
     }
 }
