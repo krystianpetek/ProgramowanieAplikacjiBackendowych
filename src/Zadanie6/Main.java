0e36e0e194ddde476a82d3eeb0fd20650f754c3d
Krystian Petek <krystianpetek2@gmail.com>
2022-03-22 19:10:15 +0100

    kolejne

--- src/Zadanie6/Main.java
+++ src/Zadanie6/Main.java
@@ -1,7 +1,29 @@
 package Zadanie6;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+        try
+        {
+            byte[] data = Files.readAllBytes(Paths.get("multipleLineFile.txt"));
+            String text = new String(data, StandardCharsets.UTF_8);
+            var podzielone = text.split("\r\n");
+
+            for(int i = 0;i< podzielone.length;i++)
+            {
+                System.out.println(i+1 + ". " + podzielone[i]);
+            }
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+
     }
 }
