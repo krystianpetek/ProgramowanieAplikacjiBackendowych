cd6d722e5e2e514b658850573a3916e2738adb0c
Krystian Petek <krystianpetek2@gmail.com>
2022-03-23 21:03:03 +0100

    Zadania skończone

--- Laboratorium01/src/Dodatkowe3/Main.java
+++ Laboratorium01/src/Dodatkowe3/Main.java
@@ -1,5 +1,23 @@
 package Dodatkowe3;
 
-public class Main {
+import java.util.HashMap;
 
+public class Main {
+    public static void main(String[] x)
+    {
+        String[] lista = new String[]{"a","a","a","b","c","d","e","f","g","h","i","j","k","k","k","k","l","m","n","o","p","r","s","t","u","w","x","y","z","z"};
+        HashMap<String, Integer> hashMapa = new HashMap<String, Integer>();
+        for(int i = 0;i<lista.length;i++){
+            if(hashMapa.containsKey(lista[i]))
+            {
+                Integer ilosc = hashMapa.get(lista[i]);
+                ilosc++;
+                hashMapa.replace(lista[i],ilosc);
+            }
+            else{
+                hashMapa.put(lista[i],1);
+            }
+        }
+        System.out.println(hashMapa);
+    }
 }
