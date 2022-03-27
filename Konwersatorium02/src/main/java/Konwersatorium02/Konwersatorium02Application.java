4da0d7bc38c09f43aa208971504b98c45837ca2a
Krystian Petek <krystianpetek2@gmail.com>
2022-03-27 11:36:47 +0200

    kon

---
+++ Konwersatorium02/src/main/java/Konwersatorium02/Konwersatorium02Application.java
@@ -0,0 +1,13 @@
+package Konwersatorium02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Konwersatorium02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Konwersatorium02Application.class, args);
+	}
+
+}
---
+++ Konwersatorium02/src/test/java/Konwersatorium02/Konwersatorium02ApplicationTests.java
@@ -0,0 +1,13 @@
+package Konwersatorium02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Konwersatorium02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
