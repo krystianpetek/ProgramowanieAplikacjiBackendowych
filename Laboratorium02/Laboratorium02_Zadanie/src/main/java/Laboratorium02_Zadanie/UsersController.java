4e84a87ef760f0c1cffb9012ecfac9caf44ea473
Krystian Petek <krystianpetek2@gmail.com>
2022-04-09 22:21:46 +0200

    update zadania i lab03

--- Laboratorium02/Laboratorium02_Zadanie/src/main/java/Laboratorium02_Zadanie/UsersController.java
+++ Laboratorium02/Laboratorium02_Zadanie/src/main/java/Laboratorium02_Zadanie/UsersController.java
@@ -6,32 +6,26 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Controller
 public class UsersController {
-    private Map<Integer, UserEntity> users = new HashMap<>();
+    private final Map<Integer, UserEntity> users = new HashMap<>();
     private Integer licznik = 1;
 
     @GetMapping("/users")
     @ResponseBody
     public Object getUsers() {
-        List<UserEntity> lista = new ArrayList<>();
-        for (UserEntity val : users.values()) {
-            lista.add(val);
-        }
-        return lista;
+        return users.values();
     }
 
     @GetMapping("/users/{id}/get")
     @ResponseBody
     public Object getUser(@PathVariable Integer id) {
-        for (Integer key : users.keySet()) {
-            if (users.containsKey(id))
-                return users.get(id);
+        var user = users.get(id);
+        if (user != null) {
+            return user;
         }
         return "Nie znaleziono użytkownika";
     }
@@ -43,23 +37,17 @@ public class UsersController {
             @RequestParam String imie,
             @RequestParam String nazwisko,
             @RequestParam Integer wiek) {
-
-        if (imie != null && nazwisko != null && wiek != 0) {
-            UserEntity uzytkownik = new UserEntity(imie, nazwisko, wiek);
-            users.put(licznik++, uzytkownik);
-            return true;
-        }
-        return false;
+        UserEntity uzytkownik = new UserEntity(imie, nazwisko, wiek);
+        users.put(licznik++, uzytkownik);
+        return true;
     }
 
     @GetMapping("/users/{id}/remove")
     @ResponseBody
     public boolean removeUser(@PathVariable Integer id) {
-        for (Integer key : users.keySet()) {
-            if (users.containsKey(id)) {
-                users.remove(id);
-                return true;
-            }
+        if (users.containsKey(id)) {
+            users.remove(id);
+            return true;
         }
         return false;
     }
---
+++ Laboratorium03/src/test/java/Laboratorium03/Laboratorium03ApplicationTests.java
@@ -0,0 +1,13 @@
+package Laboratorium03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laboratorium03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
