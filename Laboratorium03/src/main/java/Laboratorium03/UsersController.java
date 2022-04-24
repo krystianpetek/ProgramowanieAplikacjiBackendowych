c48a2d628f9356acbb5b661bf7952c10fab1f16a
Krystian Petek <krystianpetek2@gmail.com>
2022-04-24 12:37:55 +0200

    repair pagination, finally works

--- Laboratorium03/src/main/java/Laboratorium03/UsersController.java
+++ Laboratorium03/src/main/java/Laboratorium03/UsersController.java
@@ -1,6 +1,5 @@
 package Laboratorium03;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,15 +7,8 @@ import java.util.*;
 
 @Controller
 public class UsersController {
-
-    @RequestMapping("/")
-    @ResponseBody
-    public Object index() {
-        return "Hello World!";
-    }
-
-    private ArrayList<UserEntityDTO> users = new ArrayList<>();
-    private Integer count = 1;
+    private final ArrayList<UserEntityDTO> users = new ArrayList<>();
+    private Integer count = 0;
 
     @GetMapping("api/users")
     @ResponseBody
@@ -37,56 +29,53 @@ public class UsersController {
         if(users.size() <= pageSize)
             pagesCount = 1;
         else
-         pagesCount = Math.ceil(users.size()/ pageSize);
+            pagesCount = Math.ceil(users.size()/ pageSize);
 
-        ArrayList<UserEntityDTO> usersPage = new ArrayList<UserEntityDTO>();
+        ArrayList<UserEntityDTO> usersPage = new ArrayList<>();
 
-        int start = CalcPagination(pageNumber, pageSize);
+        int start = CalcPagination(pageNumber-1, pageSize);
+        int stop = CalcPagination(pageNumber, pageSize);
 
-        for(int i = start;i<pageSize;i++)
+        for(int i = start;i<stop;i++)
         {
-            if(users.get(i) == null)
+            if(users.size() == 0)
+                break;
+            if(i == users.size())
                 break;
             usersPage.add( users.get(i));
         }
         return new UsersApi(pageNumber, (int)pagesCount, pageSize, users.size(), usersPage);
     }
 
-    private static int CalcPagination(int pageNumber, int pageSize)
-    {
-        return ((pageNumber*pageSize)-pageSize);
-    }
-
     @GetMapping("api/users/{id}/get")
     @ResponseBody
     public Object getUser(@PathVariable Integer id) {
-        var user = users.get(id);
-        if (user != null) {
-            return user;
-        }
-        return "Nie znaleziono użytkownika";
-    }
-//    http://localhost:8080/api/user/add?imie=Krystian&nazwisko=Petek&email=krystianpetek@gmail.com&wiek=23
-//    http://localhost:8080/api/user/add?imie=Teresa&nazwisko=Petek&email=teresapetek@gmail.com&wiek=52
-//    http://localhost:8080/api/user/add?imie=Józef&nazwisko=Petek&email=jozefpetek@gmail.com&wiek=50
-//    http://localhost:8080/api/user/add?imie=Patrycja&nazwisko=Petek&email=patrycjapetek@gmail.com&wiek=25
-//    http://localhost:8080/api/user/add?imie=Gabriel&nazwisko=Warchał&email=gabrielwarchal@gmail.com&wiek=29
-//    http://localhost:8080/api/user/add?imie=Janina&nazwisko=Warchał&email=janinawarchal@gmail.com&wiek=59
-//    http://localhost:8080/api/user/add?imie=Agnieszka&nazwisko=Warchał&email=agnieszkawarchal@gmail.com&wiek=35
-//    http://localhost:8080/api/user/add?imie=Krystian&nazwisko=Porębski&email=krystianporebski@gmail.com&wiek=35
-//    http://localhost:8080/api/user/add?imie=Lena&nazwisko=Porębska&email=lenaporebska@gmail.com&wiek=4
-//    http://localhost:8080/api/user/add?imie=Grzegorz&nazwisko=Warchał&email=grzegorzwarchal@gmail.com&wiek=41
+        if(users.size()== 0)
+         return "Nie znaleziono użytkownika";
 
+        var user = users.get(--id);
+        return user;
+    }
+
+//    http://localhost:8080/api/user/add?imie=Krystian&nazwisko=Petek&email=krystianpetek@gmail.com
+//    http://localhost:8080/api/user/add?imie=Teresa&nazwisko=Petek&email=teresapetek@gmail.com
+//    http://localhost:8080/api/user/add?imie=Józef&nazwisko=Petek&email=jozefpetek@gmail.com
+//    http://localhost:8080/api/user/add?imie=Patrycja&nazwisko=Petek&email=patrycjapetek@gmail.com
+//    http://localhost:8080/api/user/add?imie=Gabriel&nazwisko=Warchał&email=gabrielwarchal@gmail.com
+//    http://localhost:8080/api/user/add?imie=Janina&nazwisko=Warchał&email=janinawarchal@gmail.com
+//    http://localhost:8080/api/user/add?imie=Agnieszka&nazwisko=Warchał&email=agnieszkawarchal@gmail.com
+//    http://localhost:8080/api/user/add?imie=Krystian&nazwisko=Porębski&email=krystianporebski@gmail.com
+//    http://localhost:8080/api/user/add?imie=Lena&nazwisko=Porębska&email=lenaporebska@gmail.com
+//    http://localhost:8080/api/user/add?imie=Grzegorz&nazwisko=Warchał&email=grzegorzwarchal@gmail.com
     @GetMapping("/api/user/add")
     @ResponseBody
     public boolean addUser(
             @RequestParam String imie,
             @RequestParam String nazwisko,
-            @RequestParam String email,
-            @RequestParam Integer wiek
+            @RequestParam String email
             ) {
 
-        UserEntityDTO uzytkownik = new UserEntityDTO(count++, imie + " "+ nazwisko, email);
+        UserEntityDTO uzytkownik = new UserEntityDTO(++count, imie + " "+ nazwisko, email);
         users.add(uzytkownik);
         return true;
     }
@@ -101,4 +90,10 @@ public class UsersController {
             return true;
 
     }
+
+    private static int CalcPagination(int pageNumber, int pageSize)
+    {
+        return pageSize * pageNumber;
+    }
+
 }
