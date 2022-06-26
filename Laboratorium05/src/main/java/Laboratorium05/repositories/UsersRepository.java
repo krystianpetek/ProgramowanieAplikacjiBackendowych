ca4bcc86c897feece64a83cf86190a99ae0e808d
Krystian Petek <krystianpetek2@gmail.com>
2022-06-26 10:38:29 +0200

    Update UsersRepository.java

--- Laboratorium05/src/main/java/Laboratorium05/repositories/UsersRepository.java
+++ Laboratorium05/src/main/java/Laboratorium05/repositories/UsersRepository.java
@@ -1,4 +1,10 @@
 package Laboratorium05.repositories;
 
-public class UsersRepository {
+import Laboratorium05.entities.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsersRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByUsernameAndPassword(String username, String password);
 }
